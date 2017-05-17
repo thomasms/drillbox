c71757f1c2e3af65194a402d08ab394d67876c78
TOM <stainer.tom@gmail.com>
2017-05-17 17:02:00 +0100

    Testing out dropdown javascript

--- drillbox/static/js/dropdown_menu_change_title.js
+++ drillbox/static/js/dropdown_menu_change_title.js
@@ -1,5 +1,9 @@
-$(".dropdown-menu li a").click(function(){
-    $(".dropdown-menu").on('click', 'li a', function(){
-      $(".btn:first-child").text($(this).text());
-      $(".btn:first-child").val($(this).text());
-});    
+$(document).ready(function(){
+    var dropdown_name = "#tool_dropdown"
+
+    $(dropdown_name).on('click', 'li a', function(){
+      $(dropdown_name + " .btn:first-child").html($(this).text() + ' <span class="caret"></span>');
+      $(dropdown_name + " .btn:first-child").val($(this).text());
+   });
+
+});
